a8092c597bac038d2e0cd5ffd57c03e1a9c35c4d
AnieeG <anindita.ghosh@smartcontract.com>
2024-02-29 11:44:38 -0800

    use one multicall for all

--- integration-tests/ccip-tests/load/ccip_multicall_loadgen.go
+++ integration-tests/ccip-tests/load/ccip_multicall_loadgen.go
@@ -58,9 +58,6 @@ func NewMultiCallLoadGenerator(testCfg *testsetups.CCIPTestConfig, lanes []*acti
 		if source.String() != lanes[i].SourceChain.GetChainID().String() {
 			return nil, fmt.Errorf("all lanes should be from same network; expected %s, got %s", source, lanes[i].SourceChain.GetChainID())
 		}
-		if lanes[i].SrcNetworkLaneCfg.Multicall != multiCall {
-			return nil, fmt.Errorf("multicall address should be same for all lanes")
-		}
 	}
 	client := lanes[0].SourceChain
 	lggr := logging.GetTestLogger(testCfg.Test).With().Str("Source Network", client.GetNetworkName()).Logger()
